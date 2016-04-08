0cd2a65a87aa124f7d02194375c47353b9f8f0cc
Angie Skazka <konzeptmeister@gmail.com>
2016-04-08 20:33:25 +0200

    chore(paths): Update the way data paths are sent to Vizabi. Allows more manipulations with the location of metadata and translations from tools page. Less hardcoded stuff in Vizabi

--- client/src/js/vizabi-ws-reader.js
+++ client/src/js/vizabi-ws-reader.js
@@ -2,5 +2,10 @@ require('d3');
 
 var Vizabi = require('vizabi');
 // Setup WS location from webpack variable "WS_SERVER"
-Vizabi._globals.gapminder_paths.wsUrl = WS_SERVER;
+Vizabi._globals.ext_resources = {
+  host: WS_SERVER + "/",
+  preloadPath: 'api/vizabi/',
+  dataPath: 'api/graphs/stats/vizabi-tools'
+};      
+
 require('vizabi/build/dist/vizabi.css');
