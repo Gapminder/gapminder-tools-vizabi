a80c4ca0320e456480dcc78c72048c833e1ef91a
Angie Skazka <konzeptmeister@gmail.com>
2016-05-27 00:28:22 +0200

    Attempt to fix vizabi config

--- client/src/js/vizabi-ws-reader.js
+++ client/src/js/vizabi-ws-reader.js
@@ -7,7 +7,8 @@ var Vizabi = require('vizabi');
 Vizabi._globals.ext_resources = {
   host: WS_SERVER + '/',
   preloadPath: 'api/vizabi/',
-  dataPath: 'api/graphs/stats/vizabi-tools'
+  dataPath: 'api/graphs/stats/vizabi-tools',
+  conceptpropsPath: WS_SERVER + "/api/vizabi/metadata.json",
 };
 
 require('vizabi/build/dist/vizabi.css');
