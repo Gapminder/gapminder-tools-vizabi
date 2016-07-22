e250fc81108153e76f5493a4482f8cd3ae0cf084
Vyacheslav Panchenko <vyacheslav.panchenko@valor-software.com>
2016-07-22 17:47:49 +0300

    feat(vizabi-ws-reader): implementation of separate module vizabi-ws-reader

--- client/src/js/vizabi-ws-reader.js
+++ client/src/js/vizabi-ws-reader.js
@@ -3,12 +3,25 @@
 require('d3');
 
 var Vizabi = require('vizabi');
+
+
+// Start :: WSReader Integration
+
+var WSReader = require('vizabi-ws-reader').WSReader;
+var wsReader = new WSReader().getReader();
+Vizabi.Reader.extend("waffle", wsReader);
+
+// End :: WSReader Integration
+
+
 // Setup WS location from webpack variable "WS_SERVER"
 Vizabi._globals.ext_resources = {
   host: WS_SERVER + '/',
   preloadPath: 'api/vizabi/',
   dataPath: 'api/graphs/stats/vizabi-tools',
-  conceptpropsPath: WS_SERVER + '/api/vizabi/metadata.json'
+  //conceptpropsPath: WS_SERVER + '/api/vizabi/metadata.json'
+  conceptpropsPath: 'https://waffle-server-dev-new-ddf.gapminderdev.org/api/vizabi/metadata.json',
+  translationPath: 'https://waffle-server-dev-new-ddf.gapminderdev.org/api/vizabi/translation/en.json'
 };
 
 require('vizabi/build/dist/vizabi.css');
