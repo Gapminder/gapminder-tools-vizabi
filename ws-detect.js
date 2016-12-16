c29a30242a265814e979b51eef8bfb6cb0c017b2
georgii.rychko <georgii.rychko@valor-software.com>
2016-12-16 18:34:24 +0200

    fix(ws-detect.js): change WS url according to envs

--- ws-detect.js
+++ ws-detect.js
@@ -7,8 +7,8 @@ module.exports = function (processEnv) {
   var DEFAULT_WS_HOST_URLS = {
     local: 'http://localhost',
     production: 'https://waffle-server.gapminder.org',
-    stage: 'https://waffle-server.gapminder.org',
-    development: 'https://waffle-server.gapminder.org'
+    stage: 'https://waffle-server-stage.gapminderdev.org',
+    development: 'https://waffle-server-dev.gapminderdev.org'
   };
 
   var DEFAULT_WS_PORTS = {
